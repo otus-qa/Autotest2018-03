0df51c4f10ed237f0e81849d24e48ee7ee33fdb8
I am a teacher <iamateacher@Pavels-MBP.lan>
2018-04-27 08:05:20 +0300

    added todo comments as a home task

--- src/main/java/custom/CustomExpectedCondition.java
+++ src/main/java/custom/CustomExpectedCondition.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 
 public class CustomExpectedCondition {
 
-    //TODO
+    //TODO Implement a Color class which should have methods to convert rgb/rgba to hex.
+    // 1. Add the class to the method as an argument.
+    // 2. The converting method should run automatically depend on WebDriver instance.
     public static ExpectedCondition<Boolean> elementHasColor(WebElement element, String color) {
         //rgb/rgba -> hex
         return webDriver -> element.getCssValue("background-color").equals(color);
